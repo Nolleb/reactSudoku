9d86d2f440de684429765488266c49137ea84f5e
Roger Bellon <rbellongronnier@gmail.com>
2018-11-22 20:50:40 +0100

    add header component

---
+++ src/Components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Header extends React.Component{
+    render(){
+        return(
+            <div className="header u-margin-bottom-medium">
+                <h1 className="heading-primary u-uppercase u-margin-bottom-medium">Sudoku</h1>
+                <div className="header__control">
+                    <button className="btn u-margin-right-small">New sudoku</button>
+                    <button className="btn">Reset sudoku</button>                
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Header;
